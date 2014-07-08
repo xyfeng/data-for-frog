2ea50a3ef70dbfa0787ecd30e4cd9f5969f841ca
XY Feng <fxy0111@gmail.com>
2014-07-08 17:56:37 -0400

    add paper js

---
+++ data/test.js
@@ -0,0 +1,105 @@
+var data = {
+  nodes: {
+    top: {
+      'color': 'dd3333',
+      'shape': 'dot',
+      'label': '    Topics   '
+    },
+    pop: {
+      'color': 'ccddcc',
+      'label': 'Population'
+    },
+    eco: {
+      'color': 'ccddcc',
+      'label': 'Economic'
+    },
+    hth: {
+      'color': 'ccddcc',
+      'label': 'Health'
+    },
+    fos: {
+      'color': 'ccddcc',
+      'label': 'Food Security'
+    },
+    nut: {
+      'color': 'ccddcc',
+      'label': 'Nutrition'
+    },
+    edu: {
+      'color': 'ccddcc',
+      'label': 'Education'
+    },
+    pro: {
+      'color': 'ccddcc',
+      'label': 'Protection'
+    },
+    wsh: {
+      'color': 'ccddcc',
+      'label': 'Water, Sanitation, and Hygiene'
+    },
+    she: {
+      'color': 'ccddcc',
+      'label': 'Shelter'
+    },
+    log: {
+      'color': 'ccddcc',
+      'label': 'Logistics'
+    },
+    fun: {
+      'color': 'ccddcc',
+      'label': 'Funding'
+    },
+    hum: {
+      'color': 'ccddcc',
+      'label': 'Humanitarian Profile'
+    },
+    att: {
+      'color': 'ccddcc',
+      'label': 'Attribute'
+    },
+    geo: {
+      'color': 'ccddcc',
+      'label': 'Geographic'
+    },
+    oth: {
+      'color': 'ccddcc',
+      'label': 'Other'
+    },
+    item1: {
+      'color': 'ccccdd',
+      'label': 'item 1'
+    },
+    item2: {
+      'color': 'ccccdd',
+      'label': 'item 1'
+    },
+    item3: {
+      'color': 'ccccdd',
+      'label': 'item 1'
+    }
+  },
+  edges: {
+    top: {
+      pop: {},
+      eco: {},
+      hth: {},
+      fos: {},
+      nut: {},
+      edu: {},
+      pro: {},
+      wsh: {},
+      she: {},
+      log: {},
+      fun: {},
+      hum: {},
+      att: {},
+      geo: {},
+      oth: {}
+    },
+    pop: {
+      item1: {},
+      item2: {},
+      item3: {}
+    }
+  }
+};
